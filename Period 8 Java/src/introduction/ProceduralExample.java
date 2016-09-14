e331a0d1202586c4a6dab552d1840e1d125030a1
Student 8 <Student 8@10.8.33.178>
2016-09-14 13:52:06 -0400

    change

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -2,7 +2,7 @@ package introduction;
 
 public class ProceduralExample {
   public static void main(String[] args){
-    System.out.println("Hello, world");
+    System.out.println("Hello World!");
     //System is a class, out is field, println is method of that field
   }
 }
