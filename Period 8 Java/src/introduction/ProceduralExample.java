f08f58eb6b961c98256e72d42dcdeeb4b3ed287d
Mahin <mkhan6241@bths.edu>
2016-09-15 16:51:04 -0400

    Added more names and years.

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -7,6 +7,8 @@ public class ProceduralExample {
     //System.out.println("Hello World!");
     //System is a class, out is field, println is method of that field
 	
-	System.out.println("Hi, my name is "+names[0]+ "and I am a" +years[0]+ ".");
+	System.out.println("Hi, my name is "+names[0]+ " and I am a " +years[0]+ ".");
+	System.out.println("Hi, my name is "+names[1]+ " and I am a " +years[1]+ ".");
+	System.out.println("Hi, my name is "+names[2]+ " and I am a " +years[2]+ ".");
   }
 }
