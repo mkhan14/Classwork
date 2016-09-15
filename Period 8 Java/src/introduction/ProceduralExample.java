50953df42de8eded7d277f4e53f5ea2b0d942e32
Student 8 <Student 8@10.8.33.178>
2016-09-15 13:41:26 -0400

    Jillian says her name.

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -2,7 +2,10 @@ package introduction;
 
 public class ProceduralExample {
   public static void main(String[] args){
-    System.out.println("Hello World!");
+	String[] names = {"Jillian", "Joseph", "Jordan"};  
+    //System.out.println("Hello World!");
     //System is a class, out is field, println is method of that field
+	
+	System.out.println("Hi, my name is "+names[0]+".");
   }
 }
