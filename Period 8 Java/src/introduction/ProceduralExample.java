9b44bec755ee1e7becf11051cc1f158a0de28d19
mkhan14 <mkhan6241@bths.edu>
2016-09-15 13:52:54 -0400

    Update ProceduralExample.java

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -3,9 +3,10 @@ package introduction;
 public class ProceduralExample {
   public static void main(String[] args){
 	String[] names = {"Jillian", "Joseph", "Jordan"};  
+	String[] years = {"Senior", "Freshman", "Junior"};
     //System.out.println("Hello World!");
     //System is a class, out is field, println is method of that field
 	
-	System.out.println("Hi, my name is "+names[0]+".");
+	System.out.println("Hi, my name is "+names[0]+ "and I am a" +years[0]+ ".");
   }
 }
