75ceb723aa7e577e7074469be16bc399b670c7c1
mkhan14 <mkhan6241@bths.edu>
2016-09-22 12:39:58 -0400

    Update RandomCard.java

--- Period 8 Java/src/introduction/RandomCard.java
+++ Period 8 Java/src/introduction/RandomCard.java
@@ -4,16 +4,23 @@ public class RandomCard {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		drawCard();
-	}
-	
-	public static void drawCard(){
 		String[] suit = {"King", "Queen", "Jack", "Ace"};
 		String[] symbol = {"Diamonds", "Spades", "Hearts", "Clubs"};
 		
+		System.out.println("The "+suit[draw]+" of "+symbol[draw]);
+		//drawCard();
+	}
+	
+	public static int drawSuit(){
 		double rand = Math.random();
-		int draw = (int)(4*rand);
+		int drawSu = (int)(4*rand);
+		
+		
+	}
+	
+	public static int drawSymbol(){
+		double rand = Math.random();
+		int drawSy = (int)(4*rand);
 		
 		System.out.println("The "+suit[draw]+" of "+symbol[draw]);
 	}
