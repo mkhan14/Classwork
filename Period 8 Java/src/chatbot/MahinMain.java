908db1d6eaf516000fe8a619251f9c31b91cd87e
Mahin <mkhan6241@bths.edu>
2016-09-28 07:04:37 -0400

    Added helper method.

--- Period 8 Java/src/chatbot/MahinMain.java
+++ Period 8 Java/src/chatbot/MahinMain.java
@@ -65,8 +65,11 @@ public class MahinMain {
 		//make lowercase
 		searchString = searchString.toLowerCase(); //take this out for case-sensitive
 		keyword = keyword.toLowerCase(); //take this out for case-sensitive
+		System.out.println("The phrase is " +searchString);
+		System.out.println("The keyword is "+keyword);
 		//find first position of key word
-		int psn = searchString.indexOf(0);
+		int psn = searchString.indexOf(keyword);
+		System.out.println("The keyword was found at "+psn);
 		
 		while(psn >= 0){
 			//assume preceded and followed by space
@@ -76,22 +79,54 @@ public class MahinMain {
 			if(psn > 0){
 				before = searchString.substring(psn-1, psn); //-1 looks at the character
 				//before the word
+				System.out.println("The character before is "+before);
 			}
 			//check if there is a character after the keyword
 			if(psn+keyword.length() < searchString.length()){
-				//after = searchString(searchString.substring(psn + keyword.length(), 
-					//	searchString.substring(psn + keyword.length()+1);
+				after = searchString.substring(psn + keyword.length(),psn + keyword.length()+1);
+				System.out.println("The character after is "+after);
 			}
-			if(before.compareTo("a") < 0 && after.compareTo("a") < 0){
+			if(before.compareTo("a") < 0 && after.compareTo("a") < 0 &&
+					noNegations(searchString, psn)){
+				System.out.println("Found "+keyword+ "at "+psn);
 				return psn;	
 			}else{
 				//psn+1 is one space after our current
 				//psn, so this finds the NEXT word
 				psn = searchString.indexOf(keyword,psn+1);
+				System.out.println("Did not find "+keyword+", checking position"+psn);
 			}
 		}
 		
 		return -1;
+	}
+
+	//this is a helper method. it is a method designed for helping a larger method.
+	//because of this, helper methods are generally private because they are only
+	//used by the methods they are helping. also, when u do ur project, i expect to
+	//see helper methods because they make code more readable.
+	//tells what 30 lines of code does. checks to make sure there are no negations.
+	//return true if there are no negation words in front of psn
+	private static boolean noNegations(String searchString, int psn) {
+		// TODO Auto-generated method stub
+		//check to see if the word "no" is in front of psn
+		//check to see if there are three spaces in front, then check to see
+		//if "no" is there
+		//searchString (always lowercase)
+		if(psn - 3 >= 0 && searchString.substring(psn-3,psn).equals("no ")){
+			return false;
+		}
+		//check for not
+		if(psn - 4 >= 0 && searchString.substring(psn-4,psn).equals("not ")){
+			return false;
+		}
+		if(psn - 6 >= 0 && searchString.substring(psn-6,psn).equals("never ")){
+			return false;
+		}
+		if(psn - 4 >= 0 && searchString.substring(psn-4,psn).equals("n't ")){
+			return false;
+		}
+		return true;
 	}
 
 	public static String promptInput() {
