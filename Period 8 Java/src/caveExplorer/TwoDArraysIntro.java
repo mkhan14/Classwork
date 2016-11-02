a97ac71c41475dbf56f27859fbfd9da55f6f4089
Student 8 <Student 8@10.8.33.178>
2016-11-02 14:02:59 -0400

    minesweeper

--- Period 8 Java/src/caveExplorer/TwoDArraysIntro.java
+++ Period 8 Java/src/caveExplorer/TwoDArraysIntro.java
@@ -5,6 +5,58 @@ import java.util.Arrays;
 public class TwoDArraysIntro {
 
 	public static void main(String[] args) {
+		
+		boolean[][] mines = new boolean[6][6];
+		plantMines(mines);
+		String[][] field = createField(mines);
+		printPic(field);
+	}
+	
+	private static String[][] createField(boolean[][] mines) {
+		String[][] field = new String[mines.length][mines[0].length];
+		for(int row = 0; row < field.length; row++){
+			for(int col = 0; col < field[row].length; col++){
+				if(mines[row][col])field[row][col]="X";
+				else{
+					field[row][col] = countNearby(mines, row, col);
+				}
+			}
+		}
+		return field;
+	}
+
+	private static String countNearby(boolean[][] mines, int row, int col) {
+		//arraysoutofboundsindex ap exam obsessed
+		for(int r = row - 1; r <= row + 1; r ++){
+			for(int c = col - 1; c <= col+1; c++){
+				//check that this element exists
+				//check if its in the bounds of the arrays
+				if(r>=0 && r < mines.length && c >= 0 && c < mines[0].length){
+					
+				}
+			}
+		}
+	}
+
+	private static void plantMines(boolean[][] mines) {
+		int numberOfMines = 10;
+		while(numberOfMines > 0){
+			int row = (int)(Math.random() * mines.length);
+			int col = (int)(Math.random() * mines[0].length);
+			//this prevents the same mine being selected twice
+//			while(mines[row][col]){
+//				int row = (int)(Math.random() * mines.length);
+//				int col = (int)(Math.random() * mines[0].length);
+//			}
+			if(!mines[row][col]){
+				mines[row][col] = true;
+				numberOfMines--;
+			}
+		}
+		
+	}
+
+	public static void other(){
 		String [][] pic = new String [10][8];
 		for(int row = 0; row < pic.length; row++){
 			for(int col = 0; col < pic[row].length; col++){
@@ -25,15 +77,26 @@ public class TwoDArraysIntro {
 				pic[row][col] = "w";
 			}
 		}
+		//top row and bottom row should be an underscore
+		for(int col = 0; col < pic[0].length; col++){
+			pic[0][col] = "_";
+			pic[pic.length - 1][col] = "_";
+		}
 		
-		
-		for(String[] row: pic){
-			for(String col: row){
-				System.out.print(col);
-			}
-			System.out.println();
+		for(int row = 1; row < pic.length; row++){
+			pic[row][0] = "|";
+			pic[row][pic[0].length - 1] = "|";
 		}
 	}
+	
+	public static void printPic(String[][] pic){
+			for(String[] row: pic){
+				for(String col: row){
+					System.out.print(col);
+				}
+				System.out.println();
+			}
+		}
 	
 	public static void intro(){
 		String[] xox = {"x", "o", "x", "o", "x"};
