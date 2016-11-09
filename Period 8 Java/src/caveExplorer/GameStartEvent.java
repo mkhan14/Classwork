8345065cdd49d76952252b23a2ae36adf3ecb30d
Student 8 <Student 8@10.8.33.178>
2016-11-09 12:52:33 -0500

    more caves

--- Period 8 Java/src/caveExplorer/GameStartEvent.java
+++ Period 8 Java/src/caveExplorer/GameStartEvent.java
@@ -16,6 +16,7 @@ public class GameStartEvent implements Playable {
 	@Override
 	public void play() {
 		readSequence(SEQUENCE_1);
+		System.out.println("Come on. Tell me you like puzzles. Say yes.");
 		while(CaveExplorer.in.nextLine().toLowerCase().indexOf("yes") < 0){
 			CaveExplorer.print("C'mon! You know you like puzzles. Say yes!!");
 		}
