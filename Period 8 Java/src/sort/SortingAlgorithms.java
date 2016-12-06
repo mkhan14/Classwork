c25b0f62aa9770a46f14dcd69850e628923e9774
Mahin <mkhan6241@bths.edu>
2016-12-06 07:44:27 -0500

    merge

--- Period 8 Java/src/sort/SortingAlgorithms.java
+++ Period 8 Java/src/sort/SortingAlgorithms.java
@@ -44,13 +44,47 @@ public class SortingAlgorithms {
 	}
 	
 	public static void bubbleSort(int[] arrayToSort){
-		for(int i = 0; i < arrayToSort.length - 1; i++){
+		/*for(int i = 0; i < arrayToSort.length - 1; i++){
 			for(int j = 0; j < )
-		}
+		}*/
+		System.out.println("Bubble sort with "+Arrays.toString(arrayToSort));
+	    for(int i=0; i < arrayToSort.length-1; i++){
+	        for(int j=1; j < arrayToSort.length-i; j++){
+	            if(arrayToSort[j-1] > arrayToSort[j]){
+	                swap(arrayToSort,j-1,j);
+	                System.out.println("becomes "+Arrays.toString(arrayToSort));
+	            }
+	        }
+	System.out.println("Ends as "+Arrays.toString(arrayToSort));
+	    }
 	}
 	
-	public static void mergeSort(int[] arrayToSort){
-		
+	public static int[] mergeSort(int[] arrayToSort){
+		if(arrayToSort.length==1)return arrayToSort;
+	    int half = arrayToSort.length/2;//rounds down
+	    int[] firstHalf = new int[half];
+	    int[] secondHalf = new int[arrayToSort.length - half];
+	    for(int i=0; i<firstHalf.length; i++){
+	        firstHalf[i]=arrayToSort[i];
+	    }
+	    for(int i=0; i<secondHalf.length; i++){
+	        secondHalf[i]=arrayToSort[i+firstHalf.length];
+	    }
+	    return merge(mergeSort(firstHalf),mergeSort(secondHalf));
+	}
+	
+	public static int[] merge(int[] a, int[] b){
+	    //code not shown
+		int aLength = a.length;
+		int bLength = b.length;
+		int[] abArray = new int[aLength + bLength];
+		for(int i = 0; i < aLength - 1; i++){
+			abArray[i] = a[i]; 
+		}
+		for(int j = 0; j < bLength - 1; j++){
+			abArray[j + aLength] = b[j];
+		}
+		return abArray;
 	}
 	
 	public static void quickSort(int[] arrayToSort){
