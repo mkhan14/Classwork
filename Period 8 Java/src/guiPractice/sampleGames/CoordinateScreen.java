50d645d34b4172082f570c57ced2c5fea4862f91
Mahin <mkhan6241@bths.edu>
2016-12-13 07:24:58 -0500

    text area

--- Period 8 Java/src/guiPractice/sampleGames/CoordinateScreen.java
+++ Period 8 Java/src/guiPractice/sampleGames/CoordinateScreen.java
@@ -3,12 +3,14 @@ package guiPractice.sampleGames;
 import java.util.ArrayList;
 
 import guiPractice.Screen;
+import guiPractice.components.TextArea;
 import guiPractice.components.TextLabel;
 import guiPractice.components.Visible;
 
 public class CoordinateScreen extends Screen {
 //this is where i put label
 	private TextLabel label;
+	private TextArea paragraph;
 	
 	public CoordinateScreen(int width, int height) {
 		super(width, height);
@@ -17,9 +19,13 @@ public class CoordinateScreen extends Screen {
 
 	@Override
 	public void initObjects(ArrayList<Visible> viewObjects) {
-		// TODO Auto-generated method stub
-		label = new TextLabel(40,45,760,40,"");
+		label = new TextLabel(40,45,760,40,"Sample Text");
+		paragraph = new TextArea(40,85,760,500,
+				"This is a whole paragraph. Notice how "
+				+ "as the paragraph gets to the edge"
+				+ " of the page, a new line is created.");
 		viewObjects.add(label);//push
+		viewObjects.add(paragraph);
 	}
 
 }
