2ddc628d480bf85d1ebf00405d550e2b71f4e3c1
Student 8 <Student 8@10.8.33.178>
2016-12-21 12:50:43 -0500

    clickable graphic

--- Period 8 Java/src/guiPractice/components/ClickableGraphic.java
+++ Period 8 Java/src/guiPractice/components/ClickableGraphic.java
@@ -1,21 +1,39 @@
 package guiPractice.components;
 
-public class ClickableGraphic extends Graphic implements Clickable{
+public class ClickableGraphic extends Graphic implements Clickable {
+	private Action action;
 
-	public ClickableGraphic() {
+	public ClickableGraphic(int x, int y, double scale, String imageLocation) {
+		super(x, y, scale, imageLocation);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ClickableGraphic(int x, int y, String imageLocation) {
+		super(x, y, imageLocation);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ClickableGraphic(int x, int y, int w, int h, String imageLocation) {
+		super(x, y, w, h, imageLocation);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public boolean isHovered(int x, int y) {
-		// TODO Auto-generated method stub
-		return false;
+		return x > getX() && x < getX()+getWidth() && y > getY()
+				&& y < getY() + getHeight();
 	}
 
 	@Override
 	public void act() {
 		// TODO Auto-generated method stub
-		
+		if(action != null){
+			action.act();
+		}
+	}
+	
+	public void setAction(Action a){
+		this.action = a;
 	}
 
 }
