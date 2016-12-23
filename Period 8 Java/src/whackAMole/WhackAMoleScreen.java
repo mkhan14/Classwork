18d43e74839b8c8933573674723430e9d75273c2
Mahin <mkhan6241@bths.edu>
2016-12-22 22:18:27 -0500

    whack mole screen

--- Period 8 Java/src/whackAMole/WhackAMoleScreen.java
+++ Period 8 Java/src/whackAMole/WhackAMoleScreen.java
@@ -2,20 +2,43 @@ package whackAMole;
 
 import java.util.ArrayList;
 
+import guiPractice.components.TextLabel;
 import guiPractice.components.Visible;
 import guiPractice.sampleGames.ClickableScreen;
 
-public class WhackAMoleScreen extends ClickableScreen {
+public class WhackAMoleScreen extends ClickableScreen implements Runnable{
 
+	 private ArrayList<MoleInterface> moles;
+	 private PlayerInterface player;
+	 private TextLabel label;
+	 private TextLabel timeLabel;
+	 private double timeLeft;
+	
 	public WhackAMoleScreen(int width, int height) {
 		super(width, height);
+		timeLeft = 60.0;
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void initAllObjects(ArrayList<Visible> viewObjects) {
 		// TODO Auto-generated method stub
+		getAPlayer();
+		label = new TextLabel(0,0,0,0,"Ready...");
+		timeLabel = new TextLabel(0,0,0,0,"60.0");
+		addObject(label);
+		addObject(timeLabel);
+	}
 
+	private void getAPlayer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
