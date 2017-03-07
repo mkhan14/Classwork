6ef767924a32c2ed99fd9684d0b30b2ca167410d
Student 8 <Student 8@10.8.33.156>
2017-03-07 13:08:28 -0500

    ArrayList test

---
+++ Period 8 Java/src/arrays/Item.java
@@ -0,0 +1,25 @@
+package arrays;
+
+public class Item {
+
+	private String description;
+	private boolean purchased;
+	
+	public Item(String description) {
+		this.description = description;
+		purchased = false;
+	}
+	
+	public void setPurchased(boolean b){
+		this.purchased = b;
+	}
+	
+	public boolean isPurchased(){
+		return purchased;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+}
